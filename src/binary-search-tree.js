e53952ff8b94c92d90ef6fe9d1756796e1afd2d3
trenkenshu <trenkenshu@gmail.com>
2022-10-16 17:17:05 +0300

    feat: queue

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -9,31 +9,38 @@ const { NotImplementedError } = require('../extensions/index.js');
 class BinarySearchTree {
 
   root() {
-    
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 
   add(/* data */) {
-
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 
   has(/* data */) {
-   
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 
   find(/* data */) {
-    
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 
   remove(/* data */) {
-    
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 
   min() {
-    
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 
   max() {
-    
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 }
 
--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,23 +13,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+ class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    getUnderlyingList() {
+      return this.queue;
+    }
+  
+    enqueue(value) {
+      if(typeof this.queue === 'undefined') {
+          this.queue = new ListNode(value);
+      } else {
+          let stop = false;
+          let current = this.queue;
+          for(let i = 0; !stop; i++ ) {
+            if(current.next == null) {
+                current.next = new ListNode(value);
+                stop = true;
+            } else {
+                current = current.next;
+            }
+          }  
+      }
+      return this.queue;
+    }
+  
+    dequeue() {
+        let current = this.queue;
+        let ans = current.value;
+        this.queue = current.next;
+        return ans;
+    }
   }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
 
 module.exports = {
   Queue
