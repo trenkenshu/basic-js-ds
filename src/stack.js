6b6867a711ac4d972019842ca0bc44b0b888ff61
trenkenshu <trenkenshu@gmail.com>
2022-10-16 16:41:15 +0300

    feat: stack

--- src/stack.js
+++ src/stack.js
@@ -13,20 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    push(elem) {
+        if(typeof this.stack === 'undefined') {
+            this.stack = [];        
+        } 
+        this.stack.push(elem);
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    }
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    pop() {
+        return this.stack.pop();
+    }
+
+    peek() {
+        return this.stack[this.stack.length - 1];
+    }
 }
 
 module.exports = {
