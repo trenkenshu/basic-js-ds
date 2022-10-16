e6e93149dc45d39da4d594050b275d0d71645408
trenkenshu <trenkenshu@gmail.com>
2022-10-16 20:43:24 +0300

    feat: partial BST

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -8,41 +8,137 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    root() {
+        if(typeof this.tree === 'undefined') {
+            return null;
+            
+      }
+      return this.tree;
+    }
+ ////////////////////// add ///////////////////// 
+    add( data ) {
+        if(typeof this.tree === 'undefined' ) {
+            this.tree = {
+                data: data,
+                left: null,
+                right: null
+            };
+            return this.tree;
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+        } else if (this.tree.data === null) {
+            this.tree.data = data;
+            return this.tree;
+        };
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+        let nodeToPaste = {
+            data: data,
+            left: null,
+            right: null
+        }
+        
+       // let current = this.tree;
+        function paste(tree, newNode) {
+           // c('The tree before iter :');
+            //c(tree);
+            
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+            if(tree === null) {
+                tree = {
+                    data: newNode.data,
+                    left: null,
+                    right: null
+                };
+                
+                return tree;
+            }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+            if(tree.data === newNode.data) {
+                return tree;
+            }
+            
+            //c('Current data :');
+            //c(tree.data);
+            //c('Want to paste data \n'+ newNode.data);
+            if( tree.data > newNode.data) {
+                
+                tree.left = paste(tree.left, newNode);
+                return tree;
+            } else {
+                tree.right = paste(tree.right, newNode);
+                return tree;
+            }
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+        }
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+        
+
+        return  paste(this.tree, nodeToPaste);
+    }
+  ///////////////// has /////////////////////
+    has(iWonder) {
+        //if(this.tree.data === iWonder) return true;
+        
+        function findMe( subTree, search ) {
+            let ans ;
+            if(subTree.data === search) {
+                return true;
+            }
+
+            if(subTree.data > search) {
+                if(subTree.left === null) return false;
+                ans = findMe(subTree.left, search);
+            } else {
+                if(subTree.right === null) return false;
+                ans = findMe(subTree.right, search);
+            }
+
+            return ans;
+
+        }
+        
+        return findMe(this.tree, iWonder);    
+    }
+  ///////////////// find ///////////////////
+    find( lookFor ) { 
+        if(this.tree.data === lookFor) return this.tree;
+
+        function findMe( subTree, search ) {
+            let ans;
+            //c(subTree.data+' '+search);
+            if(subTree.data === search) {
+                return subTree;
+            }
+
+            if(subTree.data > search) {
+                if(subTree.left === null) return null;
+                ans = findMe(subTree.left, search);
+            } else {
+                if(subTree.right === null) return null;
+                ans = findMe(subTree.right, search);
+            }
+
+            return ans;
+
+        }
+
+        
+
+        return  findMe(this.tree, lookFor);;      
+      
+    }
+  
+    remove(/* data */) {
+      
+    }
+  
+    min() {
+      
+    }
+  
+    max() {
+      
+    }
   }
-}
 
 module.exports = {
   BinarySearchTree
